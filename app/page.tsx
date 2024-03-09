c0dbe12afba3a897cecdefe91fc3bc7f9f2f77c0
mayur phumatiya <mayurphumatiya13@gmail.com>
2024-03-09 15:33:21 +0530

    UI updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 'use client'
-import Calendar from "./components/calendar";
+
+import Calendar from "@mayurphumatiya/calendar-ts"
 
 export default function Home() {
 
-  const getdate = (selectedDate: Date) =>{
+  const getSelectedDate = (selectedDate: Date) =>{
     console.log("CLICKED DATE",selectedDate)
   }
 
  return (
-  <>
-  <Calendar getCurrDate={getdate} />
-  </>
+  <div className="w-full h-full">
+    <h1 className="text-5xl text-center py-2 text-pink-600">Calendar-ts</h1>
+    <Calendar getCurrDate={getSelectedDate} />
+  </div>
  )
 }
